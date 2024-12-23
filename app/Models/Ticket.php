2898d93da17f19572feb54cc93512f31458709f0
iJordan99 <34099612+iJordan99@users.noreply.github.com>
2024-12-23 19:13:44 +1100

    Adds event logs to tickets and users

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use App\Enums\StatusEnum;
 use App\Http\Filters\V1\QueryFilter;
+use App\Observers\ModelObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+#[ObservedBy([ModelObserver::class])]
 class Ticket extends Model
 {
     use HasFactory;

14ce8b675c84ebd29b66def0c440f1aedb1bce0f
iJordan99 <34099612+iJordan99@users.noreply.github.com>
2025-06-25 16:25:14 +0100

    Makes types fillable via requests

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -19,14 +19,15 @@ class Ticket extends Model
     use HasFactory;
 
     const priorityMap = [
-        'low' => 1,
-        'medium' => 2,
-        'high' => 3,
+        'Low' => 1,
+        'Medium' => 2,
+        'High' => 3,
     ];
     protected $fillable = [
         'user_id',
         'title',
         'description',
+        'type',
         'priority',
         'status',
         'reproduction_step',
@@ -59,11 +60,10 @@ class Ticket extends Model
 
     public function engineer(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'assigned_tickets', 'ticket_id', 'user_id')
-            ->withTimestamps();
+        return $this->belongsToMany(User::class, 'assigned_tickets', 'ticket_id', 'user_id');
     }
 
-    public function comments(): HasMany
+    public function comment(): HasMany
     {
         return $this->hasMany(Comment::class);
     }

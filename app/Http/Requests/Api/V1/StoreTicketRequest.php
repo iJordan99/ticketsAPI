4b3ef246319abd4674f71d5ce4f7646c4aca0970
iJordan99 <34099612+iJordan99@users.noreply.github.com>
2025-09-05 15:06:57 +0100

    changes the error code and reproduction fields to be consistent  across the api

--- app/Http/Requests/Api/V1/StoreTicketRequest.php
+++ app/Http/Requests/Api/V1/StoreTicketRequest.php
@@ -36,8 +36,8 @@ class  StoreTicketRequest extends BaseTicketRequest
             'data.attributes.status' => 'required|string|size:1|in:A,C,H,X,N',
             'data.attributes.type' => 'required|string|in:incident,problem,question,request',
             'data.attributes.priority' => $isTicketsController ? 'nullable|string|in:low,medium,high' : 'required|string|in:low,medium,high',
-            'data.attributes.reproductionStep' => 'sometimes|string',
-            'data.attributes.errorCode' => 'sometimes|string',
+            'data.attributes.reproduction_step' => 'sometimes|string',
+            'data.attributes.error_code' => 'sometimes|string',
         ];
 
         if ($isTicketsController) {
